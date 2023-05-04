7d60579e129bb3086d6831dc8d4bea217c195019
home <priya595k@gmail.com>
2023-05-04 09:35:28 -0400

    initial commit

---
+++ jsfile.js
@@ -0,0 +1,12 @@
+let episode = {
+    title: 'Dark Beginnings',
+    duration: 45,
+    hasBeenWatched: false
+  };
+  
+  // Create variables here
+  // =====================================
+  console.log(episode.title);
+  console.log(episode.duration);
+  console.log(episode.hasBeenWatched);
+  
